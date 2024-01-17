331dca9d0bcfc81876d1a8b02b22373fb82b230d
Yunhui-Gao <gaoyh99@outlook.com>
2024-01-17 14:05:04 +0800

    Fix minor errors

--- src/func/func/normTVa.m
+++
@@ -1,8 +0,0 @@
-function norm = normTVa(x,tau_s,tau_t)
-
-grad = L(x);
-norm = tau_s*sum(sqrt(sum(grad(:,:,:,:,1:2).^2,1)),[2,3,4,5]) ...
-       + tau_t*sum(sqrt(sum(grad(:,:,:,:,3).^2,1)),[2,3,4,5]);
-
-end
-
